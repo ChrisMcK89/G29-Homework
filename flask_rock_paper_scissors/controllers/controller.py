a4e2fa9abf8d173bb440ea7fe4d30b152299b07a
Christopher Mckenna <christopher.mckenna123@gmail.com>
2022-01-21 17:30:45 +0000

    rps

--- flask_rock_paper_scissors/controllers/controller.py
+++ flask_rock_paper_scissors/controllers/controller.py
@@ -4,15 +4,26 @@ from rps import app
 from models.player import *
 from models.game import *
 
-@app.route('/<player_1_choice>/<player_2_choice>')
+@app.route('/welcome/<player_1_choice>/<player_2_choice>')
 def return_choice(player_1_choice, player_2_choice):
     player_1 = Player("Chris", player_1_choice)
     player_2 = Player("Steve", player_2_choice)
     game_1 = Game(player_1_choice, player_2_choice)
     result = game_1.play()
-    return render_template('result.html', title='Results', player_1=player_1, player_2=player_2, result=result)
+    def result_generator(result):
+        if result == True:
+            return f"{player_1.name} wins with {player_1.choice}"
+        elif result == False: 
+            return f"{player_2.name} wins with {player_2.choice}"
+        elif result == None:
+            return "DRAW!"
+    full_result = result_generator(result)
+
+    return render_template('result.html', title='Results', player_1=player_1, player_2=player_2, full_result=full_result)
     
-    
+@app.route('/welcome')
+def welcome_page():
+    return render_template('welcome.html', title='welcome')
 
 
 
--- flask_rock_paper_scissors/models/game.py
+++ flask_rock_paper_scissors/models/game.py
@@ -6,18 +6,18 @@ class Game():
     
     def play(self):
         if self.player_1 == "rock" and self.player_2 == "scissors":
-            return "Player 1 wins with Rock!"
+            return True
         elif self.player_1 == "rock" and self.player_2 == "paper":
-            return "Player 2 wins with paper!"
+            return False
         elif self.player_1 == "paper" and self.player_2 == "rock":
-            return "Player 1 wins with paper!"
+            return True
         elif self.player_1 == "paper" and self.player_2 == "scissors":
-            return "Player 2 wins with scissors!"
+            return False
         elif self.player_1 == "scissors" and self.player_2 == "rock":
-            return "Player 2 wins with rock"
+            return False
         elif self.player_1 == "scissors" and self.player_2 == "paper":
-            return "Player 1 wins with scissors"
+            return True
         elif self.player_1 == self.player_2:
-            return "DRAW!"
+            return None
 
 
